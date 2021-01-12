8b92ae2bc70433254321ccfecb5ba210e6d2190a
kharumax <kharumax@gmail.com>
2021-01-12 17:14:23 +0900

    First Commit

---
+++ AuthSample/AuthSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthSampleApp.swift
+//  AuthSample
+//
+//  Created by 久保田陽人 on 2021/01/12.
+//
+
+import SwiftUI
+
+@main
+struct AuthSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AuthSample
+//
+//  Created by 久保田陽人 on 2021/01/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
