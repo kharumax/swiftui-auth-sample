9449db2df6f7cf591c32558da74226b44c6676e6
kharumax <kharumax@gmail.com>
2021-01-12 22:29:11 +0900

    認証機能作成.

--- AuthSample/View/Auth/LoginView.swift
+++ AuthSample/View/Auth/LoginView.swift
@@ -9,39 +9,43 @@ import SwiftUI
 
 struct LoginView: View {
     
-    @State var email = ""
-    @State var password = ""
+    @EnvironmentObject var viewModel: AuthViewModel
     @State var isShowSignUpView = false
     
     var body: some View {
-        VStack {
-            Spacer()
-            ZStack {
-                HStack {
-                    TextField("Email", text: $email).padding()
-                }.overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1))
-            }.padding()
-            ZStack {
-                HStack {
-                    SecureField("Password",text: $password).padding()
-                }.overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1))
-            }.padding().padding(.bottom)
-            Button(action: {}, label: {
-                Text("Login").font(.system(size: 20,weight: .semibold)).foregroundColor(.black).padding()
-            }).overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1))
-            .padding(.bottom)
-            NavigationLink(
-                destination: SignUpView(isShowSignUpView: $isShowSignUpView).navigationBarHidden(true),
-                isActive: $isShowSignUpView,
-                label: {
-                    Button(action: {
-                        self.isShowSignUpView = true
-                    }, label: {
-                        Text("Go To SignUp").font(.system(size: 20,weight: .semibold)).foregroundColor(.black).padding()
-                    }).overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1))
-                    .padding(.bottom)
-                })
-            Spacer()
+        NavigationView {
+            VStack {
+                Spacer()
+                ZStack {
+                    HStack {
+                        TextField("Email", text: $viewModel.email).padding()
+                    }.overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1))
+                }.padding()
+                ZStack {
+                    HStack {
+                        SecureField("Password",text: $viewModel.password).padding()
+                    }.overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1))
+                }.padding().padding(.bottom)
+                Button(action: {
+                    viewModel.login()
+                }, label: {
+                    Text("Login").font(.system(size: 20,weight: .semibold)).foregroundColor(.black).padding()
+                }).overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1)).disabled(!viewModel.isLoginValid)
+                .padding(.bottom)
+                NavigationLink(
+                    destination: SignUpView(isShowSignUpView: $isShowSignUpView).navigationBarHidden(true),
+                    isActive: $isShowSignUpView,
+                    label: {
+                        Button(action: {
+                            print("DEBUG: Go To SignUp is clicked")
+                            self.isShowSignUpView = true
+                        }, label: {
+                            Text("Go To SignUp").font(.system(size: 20,weight: .semibold)).foregroundColor(.black).padding()
+                        }).overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1))
+                        .padding(.bottom)
+                    })
+                Spacer()
+            }
         }
     }
 }
